6c3b7a1a37a6163bda333410191419652c6b0a9e
Dimas Saputra <dimas@dicoding.com>
2022-01-16 18:53:40 +0700

    add createNewBlock

--- src/blockchain.js
+++ src/blockchain.js
@@ -3,6 +3,21 @@ class Blockchain {
     this.chain = [];
     this.newTransactions = [];
   }
+
+  createNewBlock(nonce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.newTransactions,
+      nonce,
+      hash,
+      previousBlockHash,
+    };
+
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+    return newBlock;
+  }
 }
 
 module.exports = Blockchain;
---
+++ src/blockchain.test.js
@@ -0,0 +1,36 @@
+const Blockchain = require('./blockchain');
+
+describe('Blockchain', () => {
+  it('should create blockchain instance correctly', () => {
+    const bitcoin = new Blockchain();
+
+    expect(bitcoin).toBeInstanceOf(Blockchain);
+    expect(bitcoin.chain).toEqual([]);
+    expect(bitcoin.newTransactions).toEqual([]);
+  });
+
+  describe('createNewBlock', () => {
+    it('should create new block correctly', () => {
+      const bitcoin = new Blockchain();
+
+      bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+
+      expect(bitcoin.chain).toHaveLength(1);
+      expect(bitcoin.chain[0].index).toEqual(1);
+      expect(bitcoin.chain[0].timestamp).toBeTruthy();
+      expect(bitcoin.chain[0].nonce).toEqual(2389);
+      expect(bitcoin.chain[0].hash).toEqual('78s97d4x6dsf');
+      expect(bitcoin.chain[0].previousBlockHash).toEqual('OIUOEREDHKHKD');
+    });
+
+    it('should create many new block correctly', () => {
+      const bitcoin = new Blockchain();
+
+      bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+      bitcoin.createNewBlock(2231, 'OIUASDEDHKWEW', '78s97dxww2qw');
+      bitcoin.createNewBlock(2212, 'MOAHSAJJWKASD', '123x239sj22x');
+
+      expect(bitcoin.chain).toHaveLength(3);
+    });
+  });
+});
